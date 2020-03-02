17bff8aec68a6de4fdb04924db5911d1769bf645
Petr Manek <petrmanek@users.noreply.github.com>
2020-03-02 17:34:04 +0000

    calculate absolute error between model and actual data

--- tbr_reg/visualizer.py
+++ tbr_reg/visualizer.py
@@ -89,6 +89,7 @@ class Window(QDialog):
 
         self.tbr_params = None
         self.tbr_true = None
+        self.tbr_err = None
 
         self.tbr_worker = None
         self.tbr_worker_thread = None
@@ -178,10 +179,21 @@ class Window(QDialog):
         self.tbr_params = df.reset_index()
 
         self.tbr_true = None
+        self.tbr_err = None
+
+        self.tbr_surrogate = None
+        self.evaluate_model()
 
         self.plot_model()
         self.plot_true()
         self.plot_err()
+
+    def evaluate_model(self):
+        # TODO: evaluate real model
+        self.tbr_surrogate = self.tbr_params.copy()
+        self.tbr_surrogate.insert(0, 'tbr_pred', -1.)
+        self.tbr_surrogate['tbr_pred'] = np.random.rand(
+            self.x_granularity * self.y_granularity, 1) * 2
 
     def query_tbr(self):
         if self.tbr_params is None:
@@ -205,6 +217,7 @@ class Window(QDialog):
                 run = Samplerun(no_docker=True)
                 sampled = run.perform_sample(out_file=None,
                                              param_values=self.tbr_params)
+
                 param_names = [self.x_param_name, self.y_param_name]
                 sampled = pd.merge(self.tbr_params, sampled,  how='left',
                                    left_on=param_names, right_on=param_names, suffixes=('', '_dup_'))
@@ -229,8 +242,20 @@ class Window(QDialog):
 
     def tbr_worker_samples_available(self, sampled):
         self.tbr_true = sampled
-
         self.plot_true()
+
+        err = sampled.copy()
+        err.insert(0, 'tbr_surrogate_err', 0.01)
+
+        param_names = [self.x_param_name, self.y_param_name]
+        err = pd.merge(err, self.tbr_surrogate, how='left',
+                       left_on=param_names, right_on=param_names, suffixes=('', '_dup_'))
+        err.drop([column for column in err.columns
+                  if column.endswith('_dup_')], axis=1, inplace=True)
+        err['tbr_surrogate_err'] = err.apply(
+            lambda row: np.abs(row.tbr - row.tbr_pred), axis=1)
+
+        self.tbr_err = err
         self.plot_err()
 
     def tbr_worker_finished(self):
@@ -260,7 +285,6 @@ class Window(QDialog):
                     vmax = 1 + np.abs(vmin - 1)
                 else:
                     vmin = 1 - np.abs(vmax - 1)
-                print(vmin, vmax)
 
             z_data = z_data.reshape(self.y_granularity, self.x_granularity)
             pl1 = ax1.contourf(x_data, y_data, z_data,
@@ -282,24 +306,22 @@ class Window(QDialog):
         canv.draw()
 
     def plot_model(self):
-        random_data = np.random.rand(
-            self.x_granularity * self.y_granularity, 1) * 2
+        surrogate_data = self.tbr_surrogate['tbr_pred'].to_numpy() \
+            if self.tbr_surrogate is not None else None
         self.plot_domain(self.model_fig, self.model_canv,
-                         random_data, 'Surrogate TBR')
+                         surrogate_data, 'Surrogate TBR')
 
     def plot_true(self):
         true_data = self.tbr_true['tbr'].to_numpy() \
             if self.tbr_true is not None else None
-        print(true_data)
-        print(self.tbr_true)
         self.plot_domain(self.true_fig, self.true_canv,
                          true_data, 'True TBR')
 
     def plot_err(self):
-        random_data = np.random.rand(
-            self.x_granularity * self.y_granularity, 1) * 2
+        err_data = self.tbr_err['tbr_surrogate_err'].to_numpy() \
+            if self.tbr_err is not None else None
         self.plot_domain(self.err_fig, self.err_canv,
-                         random_data, 'Approximation error',
+                         err_data, 'Approximation error',
                          symmetrical=False)
 
 
