3cb6d5683a9ca9dd919293da985d9e78d2e34126
Petr Manek <petrmanek@users.noreply.github.com>
2020-03-13 11:01:04 +0000

    add missing methods to the init

--- tbr_reg/__init__.py
+++ tbr_reg/__init__.py
@@ -1,7 +1,19 @@
+from .autoencoders import create_autoencoder
+from .autoencoders import train_autoencoder
+
 from .data_utils import load_batches
 from .data_utils import encode_data_frame
 from .data_utils import x_y_split
 from .data_utils import c_d_y_split
+
+from .model_loader import get_model_factory
+from .model_loader import load_model_from_file
+
+from .plot_params_vs_tbr import plot_params_vs_tbr
+from .plot_reg_performance import plot_reg_performance
+from .plot_utils import set_plotting_style
+from .plot_utils import density_scatter
+
 from .train import apply_on_y_columns
 from .train import fit_multiple
 from .train import predict_multiple
