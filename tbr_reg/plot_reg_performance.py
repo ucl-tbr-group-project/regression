846d1478c8b2fcb1e40ccfaa5af46aa32af7b85b
Petr Manek <petrmanek@users.noreply.github.com>
2020-03-13 10:53:01 +0000

    move density scatter to utils so that it can be reused

--- tbr_reg/plot_reg_performance.py
+++ tbr_reg/plot_reg_performance.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
-import numpy as np
+from plot_utils import density_scatter
 from sklearn.linear_model import Ridge
-from scipy.interpolate import interpn
 
 
 def plot_reg_performance(df, density_bins=80):
@@ -33,24 +32,3 @@ def plot_reg_performance(df, density_bins=80):
     ax.legend(loc='lower right')
 
     return fig, ax
-
-
-# The following utility function is courtesy of:
-# https://stackoverflow.com/questions/20105364/how-can-i-make-a-scatter-plot-colored-by-density-in-matplotlib
-def density_scatter(x, y, ax=None, sort=True, bins=20, **kwargs):
-    """
-    Scatter plot colored by 2d histogram
-    """
-    if ax is None:
-        fig, ax = plt.subplots()
-    data, x_e, y_e = np.histogram2d(x, y, bins=bins)
-    z = interpn((0.5*(x_e[1:] + x_e[:-1]), 0.5*(y_e[1:]+y_e[:-1])),
-                data, np.vstack([x, y]).T, method="splinef2d", bounds_error=False)
-
-    # Sort the points by density, so that the densest points are plotted last
-    if sort:
-        idx = z.argsort()
-        x, y, z = x[idx], y[idx], z[idx]
-
-    ax.scatter(x, y, c=z, **kwargs)
-    return ax
--- tbr_reg/plot_utils.py
+++ tbr_reg/plot_utils.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interpn
 
 
 def set_plotting_style(dark=False):
@@ -19,3 +21,24 @@ def set_plotting_style(dark=False):
 
     if dark:
         plt.style.use('dark_background')
+
+
+# The following utility function is courtesy of:
+# https://stackoverflow.com/questions/20105364/how-can-i-make-a-scatter-plot-colored-by-density-in-matplotlib
+def density_scatter(x, y, ax=None, sort=True, bins=20, **kwargs):
+    """
+    Scatter plot colored by 2d histogram
+    """
+    if ax is None:
+        fig, ax = plt.subplots()
+    data, x_e, y_e = np.histogram2d(x, y, bins=bins)
+    z = interpn((0.5*(x_e[1:] + x_e[:-1]), 0.5*(y_e[1:]+y_e[:-1])),
+                data, np.vstack([x, y]).T, method="splinef2d", bounds_error=False)
+
+    # Sort the points by density, so that the densest points are plotted last
+    if sort:
+        idx = z.argsort()
+        x, y, z = x[idx], y[idx], z[idx]
+
+    ax.scatter(x, y, c=z, **kwargs)
+    return ax
