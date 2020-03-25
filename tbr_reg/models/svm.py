54df79f1442d3767b19a58673bfca59cc93adade
Petr Manek <petrmanek@users.noreply.github.com>
2020-03-25 12:49:53 +0000

    wire up SVM parameters

--- tbr_reg/models/svm.py
+++ tbr_reg/models/svm.py
@@ -26,6 +26,15 @@ class SupportVectorModel(SKLearnModel):
     '''A support vector machine with kernel trick, implemented by SciKit.'''
 
     def __init__(self,
+                 kernel='rbf',  # linear|poly|rbf|sigmoid
+                 degree=3,
+                 gamma='scale',  # scale|auto or float
+                 coef0=0.0,
+                 tol=1e-3,
+                 C=1.0,
+                 epsilon=0.1,
+                 shrinking=True,
+                 cache_size=4096,
                  scaling='standard',  # standard|minmax|none
                  out=None,  # overrides all below
                  out_model_file=None,
@@ -37,6 +46,16 @@ class SupportVectorModel(SKLearnModel):
                               out_model_file=out_model_file,
                               out_scaler_file=out_scaler_file)
 
+        self.kernel = kernel
+        self.degree = degree
+        self.gamma = gamma
+        self.coef0 = coef0
+        self.tol = tol
+        self.C = C
+        self.epsilon = epsilon
+        self.shrinking = shrinking
+        self.cache_size = cache_size
+
     @staticmethod
     def load(model, scaler=None):
         return SKLearnModel.load(SupportVectorModel(), model, scaler=scaler)
@@ -45,10 +64,30 @@ class SupportVectorModel(SKLearnModel):
     def parse_cli_args(args):
         parser = SKLearnModel.create_cli_parser('Train support vector machine')
 
+        parser.add_argument('--kernel', type=str)
+        parser.add_argument('--degree', type=int)
+        parser.add_argument('--gamma', type=str)
+        parser.add_argument('--coef0', type=float)
+        parser.add_argument('--tol', type=float)
+        parser.add_argument('--C', type=float)
+        parser.add_argument('--epsilon', type=float)
+        parser.add_argument('--shrinking', type=bool)
+        parser.add_argument('--cache-size', type=int)
+
         return {key: value
                 for key, value in vars(parser.parse_args(args)).items()
                 if value is not None}
 
     def train(self, X_train, y_train):
-        self.sklearn_model = SVR(verbose=True)
+        self.sklearn_model = SVR(
+            verbose=True,
+            kernel=self.kernel,
+            degree=self.degree,
+            gamma=self.gamma,
+            coef0=self.coef0,
+            tol=self.tol,
+            C=self.C,
+            epsilon=self.epsilon,
+            shrinking=self.shrinking,
+            cache_size=self.cache_size)
         super(SupportVectorModel, self).train(X_train, y_train)
