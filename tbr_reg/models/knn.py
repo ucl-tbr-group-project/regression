2a5c55cb32221b3d6c94e132dc6e389d445569ca
Petr Manek <petrmanek@users.noreply.github.com>
2020-03-26 10:29:12 +0000

    wire up parameters for kNN

--- tbr_reg/models/knn.py
+++ tbr_reg/models/knn.py
@@ -26,16 +26,30 @@ class NearestNeighboursModel(SKLearnModel):
     '''A k-nearest neighbours regressor, implemented by SciKit.'''
 
     def __init__(self,
+                 n_neighbors=5,
+                 weights='uniform',  # uniform|distance
+                 algorithm='auto',  # auto|ball_tree|kd_tree|brute
+                 leaf_size=30,
+                 p=2,
+                 metric='minkowski',
+                 metric_params=None,
                  scaling='standard',  # standard|minmax|none
                  out=None,  # overrides all below
                  out_model_file=None,
-                 out_scaler_file=None
-                 ):
-        SKLearnModel.__init__(self, 'kNN', 'knn',
+                 out_scaler_file=None):
+        SKLearnModel.__init__(self, 'k-NN', 'knn',
                               scaling=scaling,
                               out=out,
                               out_model_file=out_model_file,
                               out_scaler_file=out_scaler_file)
+
+        self.n_neighbors = n_neighbors
+        self.weights = weights
+        self.algorithm = algorithm
+        self.leaf_size = leaf_size
+        self.p = p
+        self.metric = metric
+        self.metric_params = metric_params
 
     @staticmethod
     def load(model, scaler=None):
@@ -45,10 +59,25 @@ class NearestNeighboursModel(SKLearnModel):
     def parse_cli_args(args):
         parser = SKLearnModel.create_cli_parser('Train k-nearest neighbours')
 
+        parser.add_argument('--n_neighbors', type=int)
+        parser.add_argument('--weights', type=str)
+        parser.add_argument('--algorithm', type=str)
+        parser.add_argument('--leaf_size', type=int)
+        parser.add_argument('--p', type=int)
+        parser.add_argument('--metric', type=str)
+
         return {key: value
                 for key, value in vars(parser.parse_args(args)).items()
                 if value is not None}
 
     def train(self, X_train, y_train):
-        self.sklearn_model = KNeighborsRegressor()
+        self.sklearn_model = KNeighborsRegressor(
+            n_neighbors=self.n_neighbors,
+            weights=self.weights,
+            algorithm=self.algorithm,
+            leaf_size=self.leaf_size,
+            p=self.p,
+            metric=self.metric,
+            metric_params=self.metric_params
+        )
         super(NearestNeighboursModel, self).train(X_train, y_train)
