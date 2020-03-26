a686cdcf308e4b67be9d93e3f8fa7e0d66cdacc7
Petr Manek <petrmanek@users.noreply.github.com>
2020-03-26 08:50:05 +0000

    wire up parameters for GPRs

--- tbr_reg/models/gpr.py
+++ tbr_reg/models/gpr.py
@@ -26,6 +26,10 @@ class GaussianProcessModel(SKLearnModel):
     '''A Gaussian process regressor, implemented by SciKit.'''
 
     def __init__(self,
+                 alpha=1e-10,
+                 optimizer="fmin_l_bfgs_b",
+                 n_restarts_optimizer=0,
+                 random_state=0,
                  scaling='standard',  # standard|minmax|none
                  out=None,  # overrides all below
                  out_model_file=None,
@@ -37,6 +41,11 @@ class GaussianProcessModel(SKLearnModel):
                               out_model_file=out_model_file,
                               out_scaler_file=out_scaler_file)
 
+        self.alpha = alpha
+        self.optimizer = optimizer
+        self.n_restarts_optimizer = n_restarts_optimizer
+        self.random_state = random_state
+
     @staticmethod
     def load(model, scaler=None):
         return SKLearnModel.load(GaussianProcessModel(), model, scaler=scaler)
@@ -46,10 +55,21 @@ class GaussianProcessModel(SKLearnModel):
         parser = SKLearnModel.create_cli_parser(
             'Train Gaussian process regressor')
 
+        parser.add_argument('--alpha', type=float)
+        parser.add_argument('--optimizer', type=str)
+        parser.add_argument('--n-restarts-optimizer', type=int)
+        parser.add_argument('--random-state', type=int,
+                            help='seed for PRNG used in training')
+
         return {key: value
                 for key, value in vars(parser.parse_args(args)).items()
                 if value is not None}
 
     def train(self, X_train, y_train):
-        self.sklearn_model = GaussianProcessRegressor()
+        self.sklearn_model = GaussianProcessRegressor(
+            alpha=self.alpha,
+            optimizer=self.optimizer,
+            n_restarts_optimizer=self.n_restarts_optimizer,
+            random_state=self.random_state
+        )
         super(GaussianProcessModel, self).train(X_train, y_train)
