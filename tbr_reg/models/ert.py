e7275d2913696a8ac7b1a94e2e066fba8627bb03
Petr Manek <petrmanek@users.noreply.github.com>
2020-03-26 08:06:47 +0000

    wire up parameters for ERTs

--- tbr_reg/models/ert.py
+++ tbr_reg/models/ert.py
@@ -26,6 +26,20 @@ class ExtremelyRandomizedTreesModel(SKLearnModel):
     '''An extremely randomized trees regressor, implemented by SciKit.'''
 
     def __init__(self,
+                 n_estimators=10,
+                 criterion='mse',  # mse|mae,
+                 max_depth=None,
+                 min_samples_split=2,
+                 min_samples_leaf=1,
+                 min_weight_fraction_leaf=0.,
+                 max_features='auto',  # auto|sqrt|log2 or None or int or float
+                 max_leaf_nodes=None,
+                 min_impurity_decrease=0.,
+                 bootstrap=False,
+                 oob_score=False,
+                 warm_start=False,
+                 ccp_alpha=0.0,
+                 max_samples=None,
                  random_state=0,
                  scaling='standard',  # standard|minmax|none
                  out=None,  # overrides all below
@@ -38,6 +52,20 @@ class ExtremelyRandomizedTreesModel(SKLearnModel):
                               out_model_file=out_model_file,
                               out_scaler_file=out_scaler_file)
 
+        self.n_estimators = n_estimators
+        self.criterion = criterion
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.min_weight_fraction_leaf = min_weight_fraction_leaf
+        self.max_features = max_features
+        self.max_leaf_nodes = max_leaf_nodes
+        self.min_impurity_decrease = min_impurity_decrease
+        self.bootstrap = bootstrap
+        self.oob_score = oob_score
+        self.warm_start = warm_start
+        self.ccp_alpha = ccp_alpha
+        self.max_samples = max_samples
         self.random_state = random_state
 
     @staticmethod
@@ -49,6 +77,20 @@ class ExtremelyRandomizedTreesModel(SKLearnModel):
         parser = SKLearnModel.create_cli_parser(
             'Train extremely randomized trees')
 
+        parser.add_argument('--n-estimators', type=int)
+        parser.add_argument('--criterion', type=str)
+        parser.add_argument('--max-depth', type=int)
+        parser.add_argument('--min-samples-split')
+        parser.add_argument('--min-samples-leaf', type=float)
+        parser.add_argument('--min-weight-fraction-leaf')
+        parser.add_argument('--max-features')
+        parser.add_argument('--max-leaf-nodes', type=int)
+        parser.add_argument('--min-impurity-decrease', type=float)
+        parser.add_argument('--bootstrap', type=bool)
+        parser.add_argument('--oob-score', type=bool)
+        parser.add_argument('--warm-start', type=bool)
+        parser.add_argument('--ccp-alpha', type=float)
+        parser.add_argument('--max-samples')
         parser.add_argument('--random-state', type=int,
                             help='seed for PRNG used in training')
 
@@ -58,7 +100,21 @@ class ExtremelyRandomizedTreesModel(SKLearnModel):
 
     def train(self, X_train, y_train):
         self.sklearn_model = ExtraTreesRegressor(
-            verbose=True,
-            random_state=self.random_state
+            n_estimators=self.n_estimators,
+            criterion=self.criterion,
+            max_depth=self.max_depth,
+            min_samples_split=self.min_samples_split,
+            min_samples_leaf=self.min_samples_leaf,
+            min_weight_fraction_leaf=self.min_weight_fraction_leaf,
+            max_features=self.max_features,
+            max_leaf_nodes=self.max_leaf_nodes,
+            min_impurity_decrease=self.min_impurity_decrease,
+            bootstrap=self.bootstrap,
+            oob_score=self.oob_score,
+            warm_start=self.warm_start,
+            ccp_alpha=self.ccp_alpha,
+            max_samples=self.max_samples,
+            random_state=self.random_state,
+            verbose=True
         )
         super(ExtremelyRandomizedTreesModel, self).train(X_train, y_train)
